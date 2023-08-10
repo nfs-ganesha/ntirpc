2e19bb66843d37fc7bb6d30f3b0428144b58e189
Dipit Grover <dipit@google.com>
2023-08-10 14:17:34 +0000

    Add function to clear gss-security-context cache
    BUG=b/295352386
    
    Change-Id: If72c276f0bed55565ac882ac58142f3255ffb791

--- src/authgss_hash.c
+++ src/authgss_hash.c
@@ -324,3 +324,44 @@ void authgss_ctx_gc_idle(void)
 	/* perturb by 1 */
 	(void)IDLE_NEXT();
 }
+
+void authgss_ctx_hash_clear(void)
+{
+	struct rbtree_x_part *xp;
+	struct authgss_x_part *axp;
+	struct svc_rpc_gss_data *gd;
+	int ix;
+	int removed_count = 0;
+
+	authgss_hash_init();
+
+	for (ix = 0; ix < authgss_hash_st.xt.npart; ++ix) {
+		xp = &(authgss_hash_st.xt.tree[ix]);
+		axp = (struct authgss_x_part *)xp->u1;
+		mutex_lock(&xp->mtx);
+
+ again:
+		gd = TAILQ_FIRST(&axp->lru_q);
+		if (!gd)
+			goto next_t;
+
+		/* Remove entry */
+		rbtree_x_cached_remove(&authgss_hash_st.xt, xp, &gd->node_k, gd->hk.k);
+		TAILQ_REMOVE(&axp->lru_q, gd, lru_q);
+		TAILQ_INIT_ENTRY(gd, lru_q);
+		--(axp->size);
+		(void)atomic_dec_uint32_t(&authgss_hash_st.size);
+
+		/* Drop sentinel ref (may free gd) */
+		unref_svc_rpc_gss_data(gd);
+
+		++removed_count;
+		goto again;
+
+ next_t:
+		mutex_unlock(&xp->mtx);
+	}
+
+	__warnx(TIRPC_DEBUG_FLAG_RPCSEC_GSS,
+		"%s: Total authgss_ctx entries removed: %d", __func__, removed_count);
+}
