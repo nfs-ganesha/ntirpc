2ce92ae30a865bfd0f7b0b3fdf7db9b225695586
Kaleb S. KEITHLEY <kkeithle@redhat.com>
2023-09-12 09:21:01 -0400

    worker pool threads, enable setting an appropriate thread stack size
    glibc's default thread stack size is 8M. N.B. This can be overridden
    across the whole process by setting the stack size with ulimit; by default
    threads will inherit the same size for their thread stack. w
    
    Empirical testing shows that, e.g., ganesha works fine with ntirpc using
    a thread stack size as small 32K. (At 16K, NFSv4 mounts fail; the stack
    underflows.)
    
    Change-Id: If6c58eaa807ef714a8e1382741b43fcd2f763799

--- src/svc.c
+++ src/svc.c
@@ -130,7 +130,7 @@ struct work_pool svc_work_pool;
 bool
 svc_init(svc_init_params *params)
 {
-	struct work_pool_params work_pool_params;
+	struct work_pool_params work_pool_params = {0,};
 	uint32_t channels = params->channels ? params->channels : 8;
 
 	/* Initialize gtraces for libntirpc */
@@ -194,6 +194,7 @@ svc_init(svc_init_params *params)
 
 	work_pool_params.thrd_min = __svc_params->ioq.thrd_min;
 	work_pool_params.thrd_max = __svc_params->ioq.thrd_max;
+	work_pool_params.thr_stack_size = params->thr_stack_size;
 	/*
 	 * thrd_max should > channels.
 	 */
