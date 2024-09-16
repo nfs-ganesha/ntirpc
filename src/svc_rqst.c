e77dce7a377fae0642282e77e7585a9cb5a61d87
Ofir Vainshtein <ofirvins@google.com>
2024-09-16 11:23:34 +0000

    Do not release xprt on rearm fail
    This is incorrect as xioq is still in writeq which can
    cause us to release xprt multiple times for same request
    
    Change-Id: Iaf42c1bbee560acb553c446a69a5fcc5250c26ab

--- src/svc_rqst.c
+++ src/svc_rqst.c
@@ -717,7 +717,6 @@ svc_rqst_rearm_events_locked(SVCXPRT *xprt, uint16_t ev_flags)
 					sr_rec, sr_rec->id_k, sr_rec->ev_refcnt,
 					sr_rec->ev_u.epoll.epoll_fd,
 					sr_rec->sv[0], sr_rec->sv[1], code);
-				SVC_RELEASE(xprt, SVC_RELEASE_FLAG_NONE);
 			} else {
 				__warnx(TIRPC_DEBUG_FLAG_SVC_RQST |
 					TIRPC_DEBUG_FLAG_REFCNT,
@@ -757,7 +756,6 @@ svc_rqst_rearm_events_locked(SVCXPRT *xprt, uint16_t ev_flags)
 					sr_rec, sr_rec->id_k, sr_rec->ev_refcnt,
 					sr_rec->ev_u.epoll.epoll_fd,
 					sr_rec->sv[0], sr_rec->sv[1], code);
-				SVC_RELEASE(xprt, SVC_RELEASE_FLAG_NONE);
 			} else {
 				__warnx(TIRPC_DEBUG_FLAG_SVC_RQST |
 					TIRPC_DEBUG_FLAG_REFCNT,
